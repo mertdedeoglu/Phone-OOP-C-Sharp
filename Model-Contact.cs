06421560585f68c98d492c469ae814f5075bf45f
Mert Dedeoğlu <87008233+mertdedeoglu@users.noreply.github.com>
2022-06-07 19:41:09 +0300

    Create Model-Contact.cs

---
+++ Model-Contact.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mert.Models
+{
+    class Rehber
+    {
+        public string İsim { get; set; }
+        public string Soyisim { get; set; }
+        public string Numara { get; set; }
+
+        public Rehber()
+        {
+            this.İsim = "xxx";
+            this.Soyisim = "xxx";
+            this.Numara = "xxx";
+        }
+    }
+}
